89fd1e4a293c25f2d01ed3f09286f3d77b4e6128
Hellagoodcoder123 <95957618+Hellagoodcoder123@users.noreply.github.com>
2022-08-16 09:30:54 +1000

    Update Entity.java

--- com/github/hellagoodcoder123/Entity.java
+++ com/github/hellagoodcoder123/Entity.java
@@ -1,5 +1,10 @@
 package com.github.hellagoodcoder123;
 
 public class Entity {
-  
+  public boolean equal(Object x){
+    return this==x;
+  }
+  public boolean inequal(Object x){
+    return this!=x;
+  }
 }
