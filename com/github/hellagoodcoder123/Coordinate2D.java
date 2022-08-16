d22eb9729d49ed374c253c6e5c60b0e0538802f9
Hellagoodcoder123 <95957618+Hellagoodcoder123@users.noreply.github.com>
2022-08-16 12:37:15 +1000

    Create Coordinate2D.java

---
+++ com/github/hellagoodcoder123/Coordinate2D.java
@@ -0,0 +1,10 @@
+package com.github.hellagoodcoder123;
+
+public class Coordinate2D extends Entity {
+  public double x;
+  public double y;
+  public Coordinate2D(double x,double y) {
+    this.x=x;
+    this.y=y;
+  }
+}
