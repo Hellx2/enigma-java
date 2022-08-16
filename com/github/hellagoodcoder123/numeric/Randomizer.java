b6acfbb85a51ebfc5c8c0f7513a12d508be8cbd4
Hellagoodcoder123 <95957618+Hellagoodcoder123@users.noreply.github.com>
2022-08-16 13:21:03 +1000

    Update Randomizer.java

--- com/github/hellagoodcoder123/numeric/Randomizer.java
+++ com/github/hellagoodcoder123/numeric/Randomizer.java
@@ -3,13 +3,23 @@ package com.github.hellagoodcoder123.numeric;
 import com.github.hellagoodcoder123.*;
 
 public class Randomizer {
-  public static ranint(int start,int end){
+  public static int ranint(int start,int end){
+    if(end<start)throw new NumberOutOfRangeException;
+    if(end==start)return end;
     return Math.floor(Math.random()*((end+1)-start))+start;
   }
-  public static ranint(int end){
+  public static int ranint(int end){
     return ranint(0,end);
   }
-  public static ran(double min,double max){
+  public static double ran(double min,double max){
+    if(max<min)throw new NumberOutOfRangeException;
+    if(max==min)return max;
     return (Math.random()*(max-min))+min;
+  }
+  public static double ran(double max){
+    return ran(0,max);
+  }
+  public static Object randomItem(Object[] array){
+    return array[ranint(0,array.length)];
   }
 }
